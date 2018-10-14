6d252b7d36507d7a5e812b60d64981a9b6b01e85
blakereeves19 <blake.reeves19@gmail.com>
2018-10-14 01:30:48 -0500

    displayed id in divs with mouseover event, and added remove function

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ button.appendChild(buttonTxt);
 document.body.appendChild(button);
 document.body.appendChild(div);
 
-let id = 0;
-button.addEventListener('click', function(){
+let id = '0';
+button.addEventListener('click', function () {
     let squareDiv = document.createElement('div');
     div.appendChild(squareDiv);
 
@@ -18,29 +18,47 @@ button.addEventListener('click', function(){
     squareDiv.style.height = '100px';
     squareDiv.style.cssFloat = 'left';
     squareDiv.className = 'squareDivs';
+    squareDiv.setAttribute('id', id);
     id++;
 
-    squareDiv.addEventListener('mouseover', function(){
+    squareDiv.addEventListener('mouseover', function () {
         console.log('this.id');
         let squareDivTxt = document.createTextNode(this.id);
         squareDiv.appendChild(squareDivTxt);
     });
 
-    squareDiv.addEventListener('mouseout', function(){
+    squareDiv.addEventListener('mouseout', function () {
         squareDiv.innerText = '';
     });
 
-    squareDiv.addEventListener('click', function(){
+    squareDiv.addEventListener('click', function () {
         let randomColor = getRandomColor();
         squareDiv.style.backgroundColor = randomColor;
-    })
+    });
+
+    squareDiv.addEventListener('dblclick', remove);
 
 });
 
 let color = ['blue', 'green', 'purple', 'orange', 'yellow', 'red', 'pink', 'brown'];
-function getRandomColor(){
-    let randomColor = color[Math.floor(Math.random()*color.length)];
+function getRandomColor() {
+    let randomColor = color[Math.floor(Math.random() * color.length)];
     return randomColor;
 }
 
+function remove() {
+    if (this.id % 2 === 0) {
+        if (this.nextSibling) {
+            this.nextSibling.remove();
+        } else {
+            alert('No following square');
+        }
+    } else {
+        if (this.previousSibling) {
+            this.previousSibling.remove();
+        } else {
+            alert('No prior square');
+        }
+    }
+}
 //});
